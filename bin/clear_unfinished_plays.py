72c0012ae716e038efce116ed02c48a629192136
Peter Bengtsson <mail@peterbe.com>
2011-06-07 09:59:56 +0100

    clear improvements

--- bin/clear_unfinished_plays.py
+++ bin/clear_unfinished_plays.py
@@ -3,14 +3,21 @@ import os, sys
 if os.path.abspath(os.curdir) not in sys.path:
     sys.path.insert(0, os.path.abspath(os.curdir))
 
+import tornado.options
+from tornado.options import define, options
+
+define("verbose", default=False, help="be louder", type=bool)
+
 def main(*args):
+    tornado.options.parse_command_line()
     from apps.play.models import Play, PlayedQuestion
     from mongokit import Connection
     con = Connection()
     con.register([Play, PlayedQuestion])
     db = con.gkc
     unfinished = db.Play.find({'finished': None})
-    print unfinished.count(), "unfinished plays"
+    if options.verbose:
+        print unfinished.count(), "unfinished plays"
     for play in unfinished:
         play.delete()
 
@@ -20,7 +27,8 @@ def main(*args):
         if count not in (10, 20):
             play.delete()
             qless_plays += 1
-    print qless_plays, "plays with not all questions completed"
+    if options.verbose:
+        print qless_plays, "plays with not all questions completed"
 
 if __name__ == '__main__':
     import sys
