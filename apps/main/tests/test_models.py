b0ff0441b5c3c17f28ccc7d36640e48d7f0bf42d
Peter Bengtsson <mail@peterbe.com>
2011-06-08 12:18:33 +0100

    modify_date automatically updated

--- apps/main/tests/test_models.py
+++ apps/main/tests/test_models.py
@@ -1,7 +1,8 @@
 from mongokit import RequireFieldError, ValidationError
 import datetime
-#import sys; sys.path.insert(0, '..')
+from apps.main.models import UserSettings
 from base import BaseModelsTestCase
+
 
 class ModelsTestCase(BaseModelsTestCase):
 
@@ -36,8 +37,30 @@ class ModelsTestCase(BaseModelsTestCase):
         self.assertEqual(model.find({'user.$id': user._id}).count(), 1)
 
     def test_usersettings_bool_keys(self):
-        from apps.main.models import UserSettings
+
         keys = UserSettings.get_bool_keys()
         self.assertTrue(isinstance(keys, list))
         self.assertTrue(keys) # at least one
         self.assertTrue(isinstance(keys[0], basestring))
+
+    def test_updating_modify_date(self):
+        user = self.db.User()
+        user.username = u'peter'
+        user.save()
+        diff = user.modify_date - user.add_date
+        highest = max(user.modify_date, user.add_date)
+        lowest = min(user.modify_date, user.add_date)
+        diff = highest - lowest
+        milliseconds = diff.microseconds / 1000.0
+        assert milliseconds < 0.1
+
+        from time import sleep
+        sleep(0.1)
+        user.username = u'other'
+        user.save()
+
+        user = self.db.User.one({'username': u'other'})
+        self.assertTrue(user.modify_date > user.add_date)
+        diff = user.modify_date - user.add_date
+        milliseconds = diff.microseconds / 1000.0
+        self.assertTrue(diff.microseconds >= 100)
