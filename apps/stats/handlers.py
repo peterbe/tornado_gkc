556b4a49f59fd8b4d165966298bec2319732806d
Peter Bengtsson <mail@peterbe.com>
2011-06-08 08:59:03 +0100

    more stats

--- apps/stats/handlers.py
+++ apps/stats/handlers.py
@@ -17,7 +17,7 @@ class StatsHandler(BaseHandler):
         self.render('stats/index.html', **options)
 
 @route('/stats/login-method', name='stats_login_method')
-class StatsHandler(BaseHandler):
+class LoginMethodHandler(BaseHandler):
     def get(self):
         options = self.get_base_options()
         methods = defaultdict(int)
@@ -39,3 +39,39 @@ class StatsHandler(BaseHandler):
         )
         options['page_title'] = "Preferred login method"
         self.render('stats/login-method.html', **options)
+
+class Getter(dict):
+    def __getattr__(self, key):
+        return self.get(key, None)#self.__getitem__(key)
+
+
+@route('/stats/numbers', name='stats_numbers')
+class NumbersHandler(BaseHandler):
+    def get(self):
+        options = self.get_base_options()
+        facts = []
+
+        facts.append(dict(label='Registered users',
+                          number=self.db.User.find().count(),
+                          url=self.reverse_url('stats_login_method')))
+
+        facts.append(dict(label='Published questions',
+                          number=self.db.Question
+                            .find({'state':'PUBLISHED'}).count()))
+
+        facts.append(dict(label='Questions pending review',
+                          number=self.db.Question
+                            .find({'state':'ACCEPTED'}).count(),
+                          url=self.reverse_url('review_random')))
+
+        facts.append(dict(label='Finished played games',
+                          number=self.db.Play
+                            .find({'finished':{'$ne':None}}).count()))
+
+        facts.append(dict(label='Played questions',
+                          number=self.db.PlayedQuestion
+                            .find().count() / 2))
+
+        options['facts'] = [Getter(x) for x in facts]
+        options['page_title'] = "Numbers"
+        self.render('stats/numbers.html', **options)
