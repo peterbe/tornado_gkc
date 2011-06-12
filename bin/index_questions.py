a3f3724d8fbe88c9f4e7238c5520e289f4d7a991
Peter Bengtsson <mail@peterbe.com>
2011-06-12 15:47:07 +0100

    here

--- bin/index_questions.py
+++ bin/index_questions.py
@@ -2,8 +2,9 @@
 
 import code, re
 import os, sys
-if os.path.abspath(os.curdir) not in sys.path:
-    sys.path.insert(0, os.path.abspath(os.curdir))
+p = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if p not in sys.path:
+    sys.path.insert(0, p)
 
 import time
 import datetime
