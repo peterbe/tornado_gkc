57b98580fd97031817fa1a82d3aa56e6950db161
Chris West <chris@minrivertea.com>
2011-06-20 12:24:45 +0800

    styles changes

--- settings.py
+++ settings.py
@@ -23,10 +23,10 @@ WEBMASTER = 'webmaster@kwissle.com'
 ADMIN_EMAILS = ['peterbe@gmail.com', 'ashleynoval@gmail.com']
 
 TWITTER_CONSUMER_KEY = 'UcZ80RJk7x4FVWE0d93ig'
-TWITTER_CONSUMER_SECRET = open(op.join(HERE, 'twitter_consumer_secret')).read().strip()
+TWITTER_CONSUMER_SECRET = '123' # open(op.join(HERE, 'twitter_consumer_secret')).read().strip()
 
 FACEBOOK_API_KEY = '0bdf577fe3dc898fc740a3999a1f1ce0'
-FACEBOOK_SECRET = open(op.join(HERE, 'facebook_consumer_secret')).read().strip()
+FACEBOOK_SECRET = '123'  # open(op.join(HERE, 'facebook_consumer_secret')).read().strip()
 
 XAPIAN_LOCATION = op.join(HERE, 'xapian_db')
 
