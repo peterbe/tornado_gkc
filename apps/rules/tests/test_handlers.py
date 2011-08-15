58f7ebc5c1764258dfd3c45a4cdcbfd9d87e3426
Peter Bengtsson <mail@peterbe.com>
2011-08-15 13:17:10 +0100

    rules page

--- apps/rules/tests/test_handlers.py
+++ apps/rules/tests/test_handlers.py
@@ -1,3 +1,5 @@
+import re
+import datetime
 import json
 from apps.main.tests.base import BaseHTTPTestCase, TestClient
 
@@ -137,3 +139,106 @@ class HandlersTestCase(BaseHTTPTestCase):
         struct = json.loads(response.body)
         self.assertEqual(struct['questions'], 2)
         self.assertEqual(struct['with_knowledge'], 1)
+
+    def test_rules_page(self):
+        bob = self.db.User()
+        bob.username = u'bob'
+        bob.save()
+        rules = self.db.Rules()
+        rules.name = u"NAME"
+        rules.author = bob._id
+        rules.no_questions = 11
+        rules.thinking_time = 12
+        rules.save()
+
+        url = self.reverse_url('rules_page', rules._id)
+        response = self.client.get(url)
+        self.assertEqual(response.code, 200)
+        self.assertTrue(u"NAME" in response.body)
+        self.assertTrue(u'11' in response.body)
+        self.assertTrue(u'12 seconds' in response.body)
+        self.assertTrue(u'seconds ago' in response.body)
+        self.assertTrue(u'Categories: All published' in response.body)
+
+        rules.min_no_people = 3
+        rules.max_no_people = 4
+        rules.alternatives_only = True
+        rules.save()
+        url = self.reverse_url('rules_page', rules._id)
+        response = self.client.get(url)
+        self.assertEqual(response.code, 200)
+
+        self.assertTrue(u'Alternatives available all the time' in response.body)
+        self.assertTrue(u'Min. number of players: 3' in response.body)
+        self.assertTrue(u'Max. number of players: 4' in response.body)
+        self.assertTrue(u'Playable questions: 0' in response.body)
+        self.assertTrue(u'if playing against the computer: 0' in response.body)
+
+        q1 = self._create_question()
+        response = self.client.get(url)
+        self.assertTrue(u'Playable questions: 1' in response.body)
+        self.assertTrue(u'if playing against the computer: 0' in response.body)
+        self._attach_knowledge(q1)
+        response = self.client.get(url)
+        self.assertTrue(u'Playable questions: 1' in response.body)
+        self.assertTrue(u'if playing against the computer: 1' in response.body)
+
+        rules.pictures_only = True
+        rules.save()
+        response = self.client.get(url)
+        self.assertTrue(u'Playable questions: 0' in response.body)
+        self.assertTrue(u'if playing against the computer: 0' in response.body)
+
+        self._attach_image(q1)
+        response = self.client.get(url)
+        self.assertTrue(u'Playable questions: 1' in response.body)
+        self.assertTrue(u'if playing against the computer: 1' in response.body)
+
+        # when never played
+        self.assertTrue('Not been played yet' in response.body)
+
+        luke = self.db.User()
+        luke.username = u'luke'
+        luke.save()
+
+        p = self.db.Play()
+        p.users = [bob, luke]
+        p.rules = rules._id
+        p.no_players = 2
+        p.no_questions = 1
+        p.started = datetime.datetime.now()
+        p.halted = datetime.datetime.now()
+        p.save()
+
+        response = self.client.get(url)
+        self.assertTrue('Not been played yet' in response.body)
+
+        p.halted = None
+        p.finished = datetime.datetime.now()
+        p.save()
+
+        pq = self.db.PlayedQuestion()
+        pq.play = p
+        pq.question = q1
+        pq.user = bob
+        pq.right = True
+        pq.answer = u'yes'
+        pq.time = 4.3
+        pq.save()
+
+        pq = self.db.PlayedQuestion()
+        pq.play = p
+        pq.question = q1
+        pq.user = luke
+        pq.right = False
+        pq.alternatives = True
+        pq.answer = u'wrong'
+        pq.time = 2.6
+        pq.save()
+
+        response = self.client.get(url)
+        self.assertTrue('Not been played yet' not in response.body)
+        html_regex = re.compile('<.*?>')
+        body = html_regex.sub('', response.body)
+        self.assertTrue('Played 1 time' in body)
+        self.assertTrue('2 different players' in body)
